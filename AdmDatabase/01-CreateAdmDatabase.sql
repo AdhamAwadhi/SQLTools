1496d23050f01f8e81964083414bd29c13e9219c
Evgeny Khabarov <ekhabarov@dev.ms>
2015-03-29 02:22:37 +0300

    Add procs to Adm database store

--- AdmDatabase/01-CreateAdmDatabase.sql
+++ AdmDatabase/01-CreateAdmDatabase.sql
@@ -1,14 +1,14 @@
 create database Adm
 on Primary (
 	Name = 'Adm',
-	FileName = 'F:\Data\Adm.mdf',
+	FileName = 'D:\SQLData\Adm.mdf',
 	Size = 100mb,
 	MaxSize = unlimited,
 	FileGrowth = 512mb
 )
 Log on (
 	Name = 'Adm_log',
-	FileName = 'E:\Log\Adm_log.ldf',
+	FileName = 'L:\SQLLog\Adm_log.ldf',
 	Size = 100mb,
 	MaxSize = unlimited,
 	FileGrowth = 512mb
