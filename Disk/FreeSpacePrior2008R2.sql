580fd390db36b470d32c5406e82367d145fc1879
Evgeny Khabarov <ekhabarov@dev.ms>
2015-04-16 16:19:14 +0300

    Get disk free space

--- Disk/FreeSpacePrior2008R2.sql
+++ Disk/FreeSpacePrior2008R2.sql
@@ -1,1 +1,8 @@
-EXEC master..xp_fixeddrives
+declare @T table (Drive varchar(100), FreeSpaceMB int)
+insert @T
+exec master..xp_fixeddrives
+
+select	Drive,
+		FreeSpaceMB * 1. / 1024 as [FreeSpace (GB)]
+from @T
+order by Drive
