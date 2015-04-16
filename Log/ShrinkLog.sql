e666039406d968efed0c06eb657860022d58dce7
Evgeny Khabarov <ekhabarov@dev.ms>
2015-04-16 16:22:01 +0300

    Shrink log

--- Log/ShrinkLog.sql
+++ Log/ShrinkLog.sql
@@ -1,1 +1,10 @@
-DBCC SHRINKFILE (Publisher_log, truncateonly)
+use DB
+DBCC SHRINKFILE (DBName_log, truncateonly)
+
+alter database DBName
+modify file (
+	name = 'DBName_log',
+	size = 100MB,
+	filegrowth = 100MB,
+	maxsize = unlimited
+)
