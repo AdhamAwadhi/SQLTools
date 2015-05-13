a40b3f5dbe204bf77682f2b2fdca4b0a169d3cd0
Evgeny Khabarov <ekhabarov@dev.ms>
2015-05-13 17:02:22 +0300

    TempDB

--- TempDB/Tempdb_AddFiles.sql
+++ TempDB/Tempdb_AddFiles.sql
@@ -1,60 +1,66 @@
 alter database tempdb 
 modify file (
-	Name = 'tempdev',
+	Name = 'templog',
 	--newname = 'tempdev_8',	
 	--filename = 'E:\SQLData\TempDB\tempdev_8.ndf',
-	Size = 512MB,
+	Size = 1GB,
 	FILEGROWTH = 512MB,
 	MAXSIZE = unlimited
 )
 
 alter database tempdb 
 add file (
-	Name = 'tempdev_8',
-	filename = 'T:\MSSQL\Data\tempdev_8.ndf',
-	Size = 512MB,
+	Name = 'tempdev_12',
+	filename = 'Q:\SQLData\tempdev_12.ndf',
+	Size = 1GB,
 	FILEGROWTH = 512MB,
 	MAXSIZE = unlimited
 )
 
 
 declare @sql nvarchar(4000) = '',
-		@size int = 4,
+		@size int = 1,
 		@MaxLogSize int = 24,
 		@Step int = 4,
 		@MaxFileCount int = 8,
 		@CurentFileCount int = 2,
 		@FileName nvarchar(4000) 
 
-while @size <= @MaxLogSize  begin
-	set @sql =  '	alter database tempdb 
-					modify file (
-						Name = ''templog'',
-						Size = ' + convert(nvarchar(10), @size) + 'GB,
-						FILEGROWTH = 512MB,
-						MAXSIZE = unlimited
-					)'
-	exec (@sql)
-	set @size += @Step
-end
+--Step-by-step TempDB log file increase
+--while @size <= @MaxLogSize  begin
+--	set @sql =  '	alter database tempdb 
+--					modify file (
+--						Name = ''templog'',
+--						Size = ' + convert(nvarchar(10), @size) + 'GB,
+--						FILEGROWTH = 512MB,
+--						MAXSIZE = unlimited
+--					)'
+--	exec (@sql)
+--	set @size += @Step
+--end
 
 
 while @CurentFileCount <= @MaxFileCount begin
 	set @FileName = 'tempdev_' + convert(nvarchar(4000) , @CurentFileCount) 
-	set @sql = 'alter database tempdb 
-				add file (
-					Name = ' + @FileName + ',
-					filename = ''T:\Tempdb\' + @FileName + '.ndf'',
-					Size = 4GB,
-					filegrowth= 1GB,
+	set @sql = '
+alter database tempdb 
+add file (
+	Name = ' + @FileName + ',
+	filename = ''F:\DATA\' + @FileName + '.ndf'',
+	Size = 1GB,
+	filegrowth= 512MB
 					
-				)'
+);
+go'
 				--MAXSIZE = unlimited
 	print (@sql)
 	set @CurentFileCount += 1
 end
 
-select *from tempdb.sys.database_files
+select 'alter database tempdb modify file (name = ' + name +', filename = ''H:\TempDB\'+name+'.ndf'')' + char(10) + 'go' + char(10),
+'alter database tempdb modify file (name = ' + name +', size = 64MB)' + char(10) + 'go' + char(10)
+from tempdb.sys.database_files
+where physical_name like '%tempdev%'
 
 
 
