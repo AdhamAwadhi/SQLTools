3dcd2bbf42f72ade043523872b98ba51c60508a6
Evgeny Khabarov <ekhabarov@dev.ms>
2015-05-13 16:39:50 +0300

    DDL Audit

--- DDLAudit/DDLLogDatabase.sql
+++ DDLAudit/DDLLogDatabase.sql
@@ -1,18 +1,27 @@
-create database DDLLog
+declare @DataPath sysname,
+	   @LogPath sysname,
+	   @sql nvarchar(4000)
+SELECT @Datapath = convert(nvarchar(4000), SERVERPROPERTY('InstanceDefaultDataPath')),@LogPath = convert(nvarchar(4000), SERVERPROPERTY('InstanceDefaultLogPath'))
+
+
+set @sql = 'create database DDLLog
 on Primary (
-	Name = 'DDLLog',
-	FileName = 'F:\DATA\DDLLog.mdf',
+	Name = ''DDLLog'',
+	FileName = '''+ case when @@servername = 'Server' then 'D:\SQLData\' else  @DataPath end +'DDLLog.mdf'',
 	Size = 100MB,
 	MaxSize = unlimited,
 	FileGrowth = 100mb
 )
 Log on (
-	Name = 'DDLLog',
-	FileName = 'E:\LOG\DDLLog_log.ldf',
+	Name = ''DDLLog_log'',
+	FileName = ''' + case when @@servername = 'Server' then 'L:\SQLLog\' else @LogPath end + 'DDLLog_log.ldf'',
 	Size = 100mb,
 	MaxSize = unlimited,
 	FileGrowth = 100mb
-)
+)'
+
+exec (@sql )
 go
+
 alter database DDLLog set recovery simple;
 go
