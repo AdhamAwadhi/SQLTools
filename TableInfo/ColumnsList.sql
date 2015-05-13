9b51e461e00252d3899ce1a3fa04e90de8d85471
Evgeny Khabarov <ekhabarov@dev.ms>
2015-05-13 16:54:53 +0300

    Table info

--- TableInfo/ColumnsList.sql
+++ TableInfo/ColumnsList.sql
@@ -1,13 +1,20 @@
-select	s.name as SchemeName,
+select	
+		s.name as SchemeName,
 		t.name as TableName,
 		c.name as ColumnName,
 		p.name + ' (' + case when c.max_length = -1 then 'max' else convert(varchar, c.max_length) end + ')' as ColumnType,
 		c.is_nullable,
-		c.*
+		c.*,
+		'[' + c.name + '] ' +p.name + 
+				case when p.name not in ('int', 'bit', 'smallint', 'tinyint', 'datetime', 'image', 'timestamp') then ' (' + case when c.max_length = -1 then 'max' else convert(varchar, c.max_length) end + ') ' else '' end 
+				
+				+ 
+			case when c.is_nullable = 1 then ' null' else ' not null' end + ','
 from sys.tables t 
 	join sys.columns c on c.object_id = t.object_id
 	join sys.schemas s on s.schema_id = t.schema_id
 	join sys.types p on p.system_type_id = c.system_type_id and p.user_type_id = c.user_type_id
+where t.name = 'Tab4leName'
 order by s.name, t.name, c.column_id
 
 --select top 100 *
--- TableInfo/rowcount.sql
+++ TableInfo/rowcount.sql
@@ -1,12 +1,14 @@
-select	distinct 
+select	distinct
+		s.name, 
 		t.name,
 		p.rows,
-		'EXEC sp_estimate_data_compression_savings ''dbo'', '''+t.name+''', NULL, NULL, ''ROW'' ;', 
+		'EXEC sp_estimate_data_compression_savings ''dbo'', '''+t.name+''', NULL, NULL, ''PAGE'' ;', 
 		'ALTER TABLE dbo.'+t.name + ' REBUILD PARTITION = ALL WITH (DATA_COMPRESSION = PAGE); '
-from sys.tables t
-	join sys.partitions p on p.object_id = t.object_id
-where 	index_id =1
-	and t.name = 'tablename'
+from sys.tables t with(nolock)
+	join sys.schemas s  with(nolock) on s.schema_id = t.schema_id
+	join sys.partitions p  with(nolock) on p.object_id = t.object_id
+--where --index_id = 1
+		--and t.name = ''
 order by p.rows desc
 
 
